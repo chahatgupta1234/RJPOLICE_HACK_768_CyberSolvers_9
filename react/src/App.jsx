468f02a6a94d4e15d8eba79d0cb015b26efee32f
mayankkhichi <117597630+mayankkhichi@users.noreply.github.com>
2023-12-30 12:45:13 +0530

    change

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [complainantName, setComplainantName] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
+
+  const submitClaim = async () => {
+    // ... (your existing code for submitting claims)
+
+    // After submitting the claim, update the dashboard
+    // You may need to add logic to refresh the dashboard in a real-world scenario
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mt-5">
+      <h1 className="mb-4">1930 Helpline Automated Recovery Platform</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title mb-4">File a Claim</h2>
+              {/* Form for filing claims */}
+              {/* ... (your existing form code) */}
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              {/* Display case status for complainants */}
+              <h2 className="card-title mb-4">Case Status</h2>
+              <p>{statusMessage}</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Dashboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
