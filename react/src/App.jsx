de8528fc062a18bb4ad3255aae6c405b1f6565df
dixitudit <58696687+dixitudit@users.noreply.github.com>
2023-12-30 14:31:56 +0530

    changes1

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,45 +1,18 @@
-import React, { useState } from 'react';
+import Login from './Login';
+import {Route, Routes} from 'react-router-dom';
 import Dashboard from './Dashboard';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
+axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
-  const [complainantName, setComplainantName] = useState('');
-  const [statusMessage, setStatusMessage] = useState('');
-
-  const submitClaim = async () => {
-    // ... (your existing code for submitting claims)
-
-    // After submitting the claim, update the dashboard
-    // You may need to add logic to refresh the dashboard in a real-world scenario
-  };
 
   return (
-    <div className="container mt-5">
-      <h1 className="mb-4">1930 Helpline Automated Recovery Platform</h1>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="card-title mb-4">File a Claim</h2>
-              {/* Form for filing claims */}
-              {/* ... (your existing form code) */}
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              {/* Display case status for complainants */}
-              <h2 className="card-title mb-4">Case Status</h2>
-              <p>{statusMessage}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Dashboard />
-    </div>
-  );
+    <Routes>
+      <Route index element={<Login/>} />
+      <Route path="/dash" element={<Dashboard/>} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ react/src/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Login() {
+  const [complaintNo, setComplaintNo] = useState('');
+  // const [statusMessage, setStatusMessage] = useState('');
+
+  function submitClaim(ev){
+    // ... (your existing code for submitting claims)
+    ev.preventDefault();
+    console.log("submit");
+    window.location.href = '/dash';
+    // After submitting the claim, update the dashboard
+    // You may need to add logic to refresh the dashboard in a real-world scenario
+  }
+
+  return (
+    <div className="container mt-5">
+
+      <h1>1930 Helpline Automated Recovery Platform</h1>
+      <div id="fileClaimForm">
+        <label htmlFor="complaintNo">Complainant Number:</label>
+        <input type="number" id="complaintNo" onChange={ev => setComplaintNo(ev.target.value)} value={complaintNo}/>
+        <button onClick={submitClaim}>Submit Claim</button>
+      </div>
+      {/* <div id="caseStatus">
+        <h2>Case Status</h2>
+        <p id="statusMessage"></p>
+      </div> */}
+    </div>
+  );
+}
+
+export default Login;
