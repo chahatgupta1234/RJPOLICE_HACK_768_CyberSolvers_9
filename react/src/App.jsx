7b1012100a41e427416b9478f2efbcf963bc6ecb
Chahat Gupta <chahatguptanit@gmail.com>
2024-01-09 14:44:01 +0530

    add tailwind

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,6 +6,33 @@ import axios from "axios";
 axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
+//   return (
+//     <div className="container">
+//      {/* Header section */}
+//       <header>
+//         {/* Logo and title */}
+//         <button>Share</button>
+//       </header>
+
+//       {/* Main content */}
+//       <main>
+//         <h1>Fraud Detection by Rajasthan Police</h1>
+//         <h2>The AI Solution you need to protect from any fraud.</h2>
+//         <button>Open</button>
+
+//         <section>
+//           <h3>Hello everyone! Here is a broad Solution for any type of Mislead or Fraud</h3>
+//           <p>Presented by Rajasthan Police on 1930 Helpline</p>
+//         </section>
+//       </main>
+
+//       {/* Footer */}
+//       <footer>
+//         <p>Rajasthan Police on 1930 Helpline</p>
+//       </footer>
+//     </div>
+//   );
+
 
   return (
     <Routes>
@@ -13,6 +40,7 @@ function App() {
       <Route path="/dash" element={<Dashboard/>} />
     </Routes>
   )
+
 }
 
 export default App
